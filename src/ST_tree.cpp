d0f986a021424bba9e64814e1a27ed1de1b34acb
dimsplendid <dimsplendid@gmail.com>
2017-03-24 09:47:13 +0800

    modified the output of tree

--- src/ST_tree.cpp
+++ src/ST_tree.cpp
@@ -1,7 +1,66 @@
 #include "ST_tree.h"
+link_lst * creat_node(int data){
+	// dynamic allocate memory
+  printf("creat node...\n");
+  printf("data is: %d\n",data);
+
+	link_lst * n = (link_lst*)malloc(sizeof(link_lst));
+	n->data = data;
+  n->next = NULL;
+  return n;
+}
+
+void insert_node(link_lst * n1, link_lst * n2) {
+  n2->next = n1->next;
+  n1->next = n2;
+}
+
+void remove_node(link_lst * n1) {
+  n1->next = n1->next->next;
+}
+
+void link_lst_printf (link_lst * lst) {
+  link_lst * n = lst;
+  printf("members: ");
+  while (n != NULL) {
+    printf("%d ", n->data);
+    n = n->next;
+  }
+  printf("\n");
+}
+void link_lst_fprintf(const char * filename, link_lst * lst){
+  link_lst * n = lst;
+  FILE * f = fopen(filename,"a");
+  if (f == NULL){ perror("Error opening file!\n"); }
+  else{
+    while (n != NULL) {
+      fprintf (f, "%d ",n->data);
+      n = n->next;
+    }
+    fprintf(f, "\n");
+    fclose(f);
+  }
+}
+
+void add_member(link_lst * lst, int data){
+  link_lst * now = lst;
+  while (now->next != NULL){
+    now = now->next;
+  }
+  link_lst * new_member = creat_node(data);
+  now->next = new_member;
+}
+
+void free_link_lst(link_lst * lst) {
+    if (lst->next != NULL) {
+        free_link_lst(lst->next);
+    }
+    free(lst);
+}
+
 tree * insertnode(){
   // initialized the node of tree
-  printf("insertnode... \n");
+  // printf("insertnode... \n");
   tree * newnode;
   newnode = (tree *)malloc(sizeof(tree));
   newnode->id = 0;
@@ -11,32 +70,97 @@ tree * insertnode(){
   newnode->r_tree = NULL;
   newnode->max_flw = 0.0;
   newnode->size=0;
+  // newnode->members_modified = creat_node(0);
   return newnode;
 }
-void tree_printf(tree * root){
-  // print the tree under the root
-  if (root != NULL) {
-    tree * lt = root->l_tree;
-    tree * rt = root->r_tree;
-    /*
-    printf("Node: ");
-    for (int i = 0; i < root->members.size();i++){
-      printf("%s ",root->members[i].c_str());
+
+int tree_cluster(tree * root){
+  return 0;
+}
+// print tree on terminal
+void print_member(tree * root){
+  if (root != NULL){
+    if ((root->l_tree == NULL) && (root->r_tree == NULL)){
+      printf (" %d",root->members[0]);
     }
-    */
-    // printf("test members: %s\n",root->members.at(0).c_str());
-    printf("Rank: %d Id: %d\n",root->rank,root->id);
-    printf("%.2f\n",root->max_flw);
-    printf("members: \n");
-    for (int i = 0; i < root->size; i++) {
-      printf("%d ",root->members[i]);
+    else{
+      print_member(root->l_tree);
+      print_member(root->r_tree);
     }
-    printf("\n");
-    tree_printf(lt);
-    tree_printf(rt);
   }
 }
-int tree_cluster(tree * root){
+void tree_printf(tree * root){
+  if(root != NULL) {
+    printf("Rank: %d Id: %d\n",root->rank,root->id);
+    printf("maximum flow: %.2f\n",root->max_flw);
+    printf("members: ");
+    print_member(root);
+    printf("\n");
+    tree_printf(root->l_tree);
+    tree_printf(root->r_tree);
+  }
+}
+
+// print tree to file
+void tree_get_member(tree * root, link_lst * members){
+  link_lst * n = members;
+  if (root != NULL){
+    if ((root->l_tree == NULL) && (root->r_tree == NULL)){
+      if(n->data == -1){
+        printf("set n->data = member\n" );
+        n->data = root->members[0];
+      }
+      else{
+        printf("call add_member()\n" );
+        add_member(n,root->members[0]);
+      }
+      printf("%d ",root->members[0]);
+    }
+    else{
+      tree_get_member(root->l_tree,n);
+      tree_get_member(root->r_tree,n);
+    }
+  }
+}
+
+void tree_fprintf( const char * filename, tree * root){
+  if(root != NULL) {
+    link_lst * root_members = creat_node(-1);
+    // link_lst * lt_members = NULL;
+    // link_lst * rt_members = NULL;
+
+    tree_get_member(root, root_members);
+    link_lst_fprintf(filename,root_members);
+    link_lst_printf(root_members);
+
+    free_link_lst(root_members);
+
+    tree_fprintf(filename, root->l_tree);
+    tree_fprintf(filename, root->r_tree);
+
+  }
+}
+
+void tree_printf2file( const char * filename, tree * root){
+  FILE * f = fopen(filename,"w");
+  if (f == NULL) { perror("Error opening file!\n"); }
+  else {fclose(f);}
+
+  f = fopen(filename,"a");
+  if (f == NULL) { perror("Error opening file!\n"); }
+
+  FILE * tmp1 = fopen("Tree_first.dot","r");
+  char sentence[256] ;
+  if (tmp1 == NULL){ perror("Error opening file!\n");}
+  else{
+    while (fgets(sentence,256,tmp1) != NULL){
+        fputs(sentence,f);
+    }
+    fclose (tmp1);
+  }
+  fclose(f);
+
+  tree_fprintf(filename,root);
 
 }
 #ifdef MAIN
@@ -51,11 +175,13 @@ int main(){
   root->r_tree = rt;
   lt->par = root;
   rt->par = root;
+  lt->members[0]=1;
+  rt->members[0]=2;
 
-  //root->members.push_back("root");
-  //lt->members.push_back("lt");
-  //rt->members.push_back("rt");
-  tree_printf(root);
+  // tree_printf(root);
+  const char * filename = "mT.dot";
+  tree_printf2file(filename,root);
+
   return 0;
 }
 #endif
--- src/ST_tree.h
+++ src/ST_tree.h
@@ -3,6 +3,12 @@
 #include <stdlib.h>
 #include "Graph.h"
 
+// Link list to store the member of clusters
+typedef struct ns{
+    int data;
+    struct ns* next;
+} link_lst;
+
 struct st_tree {
 	int id;
 	int rank;
@@ -11,6 +17,7 @@ struct st_tree {
 	struct st_tree * r_tree;
 	double max_flw;
 	int members[100];
+	link_lst * members_modified;
 	int size;
 };
 typedef struct st_tree tree;
